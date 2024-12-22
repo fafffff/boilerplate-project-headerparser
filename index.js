fa2f26b7a1787621c051312011fe2f6c1c74fd5b
Fasika Belayneh <fafibelayneh@gmail.com>
2024-12-23 02:36:24 +0300

    Update index.js to use headerRouter instead of headerInfo and add headerRouter.js file`

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const mongoose=require("mongoose");
 const cors=require("cors");
 const path=require("path");
 const app=express();
+const headerRouter=require('./router/headerRouter');
 
 mongoose.connect(process.env.MONGO_URI).then(()=>console.log("Connected to Mongo DB"))
 .catch((err)=>console.error("Mongo connection error:",err));
 
-app.use('/Header',headerInfo);
+app.use('/Header',headerRouter);
 app.use(express.static(path.join(__dirname,'public')))
 
 app.listen(process.env.PORT||3000,()=>console.log(`server is running on port ${process.env.PORT||3000}`))
---
+++ router/headerRouter.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const router=express.Router();
+const Header=require('../model/headerSchema')
+router.get('/:Router',async(req,res)=>{
+const ipAddress=req.ip||req.connection.remoteAddress;
+const language=req.headers['accept-language']||'Uknown';
+const software=req.headers['user-agent']||'Unknown';
+const response={
+    ipAddress:ipAddress,
+    language:language.split(',')[0],
+}
+}
+
+)
